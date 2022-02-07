f86bff732850a5fcc698861edb00fdbfadccd9b7
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-02-07 14:39:51 -0500

    Insert data into list_items table

---
+++ db/seeds/list_items.sql
@@ -0,0 +1,23 @@
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Wheat Flour', 1, 1);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Pasta', 1, 1);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Bread', 1, 1);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Eggs', 1, 1);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Yogurt', 1, 1);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Butter', 1, 1;
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Cooking Oil', 1, 1);
+
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Milk', 1, 2);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Cottage Cheese', 1, 2);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Eggs', 1, 2);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Coffee', 1, 2);
+
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Car Wash', 2, 3);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Clean Driveway ', 2, 3);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Clean Vacuum Filters', 2, 3);
+
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Water plants', 2, 4);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Laundry', 2, 4);
+
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Travel Bag', 3, 4);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Water Bottles', 3, 4);
+INSERT INTO list_items (item_name, list_id, user_id) VALUES ('Shoes', 3, 4);
