a536190aaa8c4d971854bbc31e0d235f0c9c4add
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2022-02-07 15:35:02 -0500

    Add event.sql file

---
+++ db/schema/events.sql
@@ -0,0 +1,5 @@
+INSERT into events ( event_name, description, account_id ) VALUES ( 'Parent Teacher meeting',"Teacher wanted to talk about Zara's special award", 1)
+
+INSERT into events (event_name,description , all_day ,
+  start_time TIME,
+  end_time TIME, is_private , address , account_id )
