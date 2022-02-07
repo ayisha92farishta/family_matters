c5e473b5fc4467239ee14cee092e06a098dc13ec
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2022-02-07 16:01:27 -0500

    Add more events

--- db/schema/events.sql
+++ db/schema/events.sql
@@ -1,5 +1,7 @@
-INSERT into events ( event_name, description, account_id ) VALUES ( 'Parent Teacher meeting',"Teacher wanted to talk about Zara's special award", 1)
+INSERT into events ( event_name, description, account_id ) VALUES ( 'Granma Birthday', 3);
 
-INSERT into events (event_name,description , all_day ,
-  start_time TIME,
-  end_time TIME, is_private , address , account_id )
+INSERT into events (event_name,description, all_day,start_time TIME, end_time TIME, account_id ) VALUES ('Parent Teacher meeting',"Teacher wanted to talk regarding Zara's grades", FALSE, '13:00:00', '14:30:00', 7);
+
+INSERT into events (event_name,all_day, start_time TIME, end_time TIME, is_private , address , account_id ) VALUES ('Graduation after Party ', FALSE, '17:00:00', '21:00:00', TRUE, 'At Pauls yatch', 1);
+
+INSERT into events (event_name,all_day,start_time TIME, end_time TIME, account_id ) VALUES ('Soccer Practice', FALSE, '12:00:00', '14:30:00', 6);
