063eaa9beb4d5bd53fa570241db810dd2ef9515b
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2022-02-07 16:44:48 -0500

    Add remaining files

--- db/seeds/05_user_lists.sql
+++ db/seeds/05_user_lists.sql
@@ -3,3 +3,4 @@ INSERT INTO user_lists (user_id, list_id, account_id) VALUES (2, 1, 2);
 INSERT INTO user_lists (user_id, list_id, account_id) VALUES (3, 2, 3); 
 INSERT INTO user_lists (user_id, list_id, account_id) VALUES (4, 2, 3); 
 INSERT INTO user_lists (user_id, list_id, account_id) VALUES (4, 3, 3); 
+
--- db/seeds/07_user_events.sql
+++ db/seeds/07_user_events.sql
@@ -1,1 +1,2 @@
 INSERT INTO user_events (user_id, event_id) VALUES (1, 3); 
+INSERT INTO user_events (user_id, event_id) VALUES (1, 3); 
