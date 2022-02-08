2f2cbfd00acaeb27efd9bdd61edf2e8b63148408
EttyDaniel <danieleti@gmail.com>
2022-02-08 05:22:20 +0000

    creating env file and updating schema and seeds

--- db/seeds/06_events.sql
+++ db/seeds/06_events.sql
@@ -1,4 +1,4 @@
-INSERT into events ( event_name) VALUES ( 'Granma Birthday');
-INSERT into events (event_name,description, all_day,start_time TIME, end_time TIME) VALUES ('Parent Teacher meeting',"Teacher wanted to talk regarding Zara's grades", FALSE, '13:00:00', '14:30:00');
-INSERT into events (event_name,all_day, start_time TIME, end_time TIME, is_private , address ) VALUES ('Graduation after Party ', FALSE, '17:00:00', '21:00:00', TRUE, 'At Pauls yatch'); -- Private event
-INSERT into events (event_name,all_day,start_time TIME, end_time TIME) VALUES ('Soccer Practice', FALSE, '12:00:00', '14:30:00');
+--INSERT into events ( event_name) VALUES ( 'Granma Birthday');
+INSERT into events (event_name, event_description, all_day,start_time, end_time) VALUES ('Parent Teacher meeting', 'welcome', FALSE, '13:00:00', '14:30:00');
+INSERT into events (event_name,all_day, start_time, end_time, is_private , event_address) VALUES ('Graduation after Party ', FALSE, '17:00:00', '21:00:00', TRUE, 'At Pauls yatch'); -- Private event
+INSERT into events (event_name,all_day,start_time, end_time) VALUES ('Soccer Practice', FALSE, '12:00:00', '14:30:00');
