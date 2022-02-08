f758311717511f3830c53b9c235fcf03d42ddc26
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2022-02-07 21:07:47 -0500

    Add recepies seed files

--- db/seeds/10_recepies.sql
+++ db/seeds/10_recepies.sql
@@ -1,4 +1,9 @@
 
-INSERT into recipes (name , preparation_time , cooking_time , serving , ingredients , instructions , account_id , user_id ) VALUES ( 'Rice', 5, 20, 4, '2 cups Rice , 4 cups Water', 'Wash rice, Add water, boil rice in water', 1 );
+INSERT into recipes (name , preparation_time , cooking_time , serving , ingredients , instructions , account_id , user_id ) VALUES ( 'Rice', 5, 20, 4, '2 cups Rice , 4 cups Water', 'Wash rice, Add water, boil rice in water', 1, 1 );
 
-INSERT into recipes (name , preparation_time , cooking_time , serving , ingredients , instructions , account_id , user_id ) VALUES ( 'Custard', 15, 40, 4, '! packet custard powder, Milk  Rice , 4 cups Water', 'Wash rice, Add water, boil rice in water', 4 );
+INSERT into recipes (name , preparation_time , cooking_time , serving , ingredients , instructions , account_id , user_id ) VALUES ( 'Custard', 15, 40, 6, '1 packet custard powder, Milk, Sugar , Salt', 'Mix everything together and heat it up', 3, 3 );
+INSERT into recipes (name , preparation_time , cooking_time , serving , ingredients , instructions , account_id , user_id ) VALUES ( 'Custard', 15, 40, 6, '1 packet custard powder, Milk, Sugar , Salt', 'Mix everything together and heat it up', 3, 3 );
+
+INSERT into recipes (name , preparation_time , ingredients , instructions , account_id , user_id ) VALUES ( 'Jello', 15,  '2 packets jello powder, water', 'Bring water to a boil, add jello powder , freeze it', 4, 5 );
+INSERT into recipes (name , preparation_time , ingredients , instructions , account_id , user_id ) VALUES ( 'Jello', 15,  '2 packets jello powder, water', 'Bring water to a boil, add jello powder , freeze it', 4, 6 );
+INSERT into recipes (name , preparation_time , ingredients , instructions , account_id , user_id ) VALUES ( 'Jello', 15,  '2 packets jello powder, water', 'Bring water to a boil, add jello powder , freeze it', 4, 7 );
