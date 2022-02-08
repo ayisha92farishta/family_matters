cb4693b4c47b15592bfefe576d684634ff5e2606
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2022-02-07 21:09:01 -0500

    minor debug

--- db/seeds/07_user_events.sql
+++ db/seeds/07_user_events.sql
@@ -5,7 +5,6 @@ INSERT INTO user_events (user_id, event_id, account_id) VALUES (3, 2, 3);
 INSERT INTO user_events (user_id, event_id, account_id) VALUES (4, 2, 3);
 --3rd event for account_id-3 -> private event, only seen by Leon
 INSERT INTO user_events (user_id, event_id, account_id) VALUES (4, 3, 3);
-
 -- 4th event, public event--therefore seen by all 3 users under the account 
 INSERT INTO user_events (user_id, event_id, account_id) VALUES (5, 4, 4); 
 INSERT INTO user_events (user_id, event_id, account_id) VALUES (6, 4, 4); 
