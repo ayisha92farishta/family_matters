afad97613c512af8426c12baf71fdf8cc057c26f
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-02-10 15:43:10 -0500

    Connect login route on frontend, move all client files into its own folder

--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,10 @@ const PORT = process.env.PORT || 8001;
 //const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const morgan = require("morgan");
-const bcrypt = require("bcrypt");
-
+// const bcrypt = require("bcrypt");
+// require cors and update. 
 // PG database client/connection setup
 const { Pool } = require("pg");
 const dbParams = require("./lib/db");
@@ -19,10 +20,17 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-
+app.use(cors());
 //app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));//??
 
+// var corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200 // For legacy browser support
+// }
+
+// app.use(cors(corsOptions));
 //COMEBACK LATER
 
 // app.use(
@@ -39,12 +47,12 @@ app.use(express.urlencoded({ extended: true }));//??
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const loginRoutes = require("./routes/login");
+const authRoutes = require("./routes/auth");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your owna
 app.use("/", usersRoutes(db));
-app.use("/login", loginRoutes(db));
+app.use("/api/auth", authRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
