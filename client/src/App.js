696ed9fb2aee0e371e0797c62778e56830d6d28e
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-02-10 23:55:45 -0500

    Render SignUpSide component for testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import SignInSide from './components/SignInSide';
+import SignUpSide from './components/SignUpSide';
 import Paperbase from './components/Homepage/Paperbase';
 
 function App() {
   return (    
-    <SignInSide />  
+    <SignUpSide />  
   );
 }
 
