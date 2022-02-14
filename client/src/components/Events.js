04abca216c908edbfb7853fc6f81033f86b4a906
EttyDaniel <danieleti@gmail.com>
2022-02-13 23:46:06 -0500

    adding big calendar and moment package

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { Component }from 'react';
 import '../App.css';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-function Events() {
-  return (    
-    <h1>This is the Events page</h1> 
-  );
+const localizer = momentLocalizer(moment);
+
+class Events extends Component {
+  state = {
+    events: [
+      {
+        // start: moment().toDate(),
+        // end: moment()
+        //   .add(1, "days")
+        //   .toDate(),
+        // title: "Some title"
+      }
+    ]
+  };
+
+ 
+
+  render() {
+    return (
+      <div className="App">
+        <Calendar selectable
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          //events={this.state.events}
+          //onSelectEvent={event => this.onEventClick(event)}
+          style={{ height: 750, marginLeft: 310, marginRight: 15 }}
+        />
+      </div>
+    );
+  }
 }
 
 export default Events;
