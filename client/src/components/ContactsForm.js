41664575c2a09192a2022cfdc86bbaa8b571ffb8
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-02-15 14:34:11 -0500

    Move the form to the center of the page and add placeholders

--- client/src/components/ContactsForm.js
+++ client/src/components/ContactsForm.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState} from 'react';
-import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -26,57 +25,35 @@ export default function ContactsForm() {
     })
   }
   return (
-    <div>
+    <div >
       <h3 className= "text-center ">New Contact</h3>
-      {/* <Form onSubmit={onSubmitForm}>
-        <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control size="lg" type="text" value={name} placeholder="Enter name" onChange={e => setName(e.target.value)}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control size="lg" type="number" value={number} placeholder="Enter number" onChange={e => setNumber(e.target.value)}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control size="lg" type="email" value={email} placeholder="Enter email" onChange={e => setEmail(e.target.value)}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicAddress">
-          <Form.Label>Address</Form.Label>
-          <Form.Control size="lg" type="address" value={address} placeholder=" Enter address" onChange={e => setAddress(e.target.value)}/>
-        </Form.Group>
-        <Button variant="primary" type="submit" onSubmit={onSubmitForm} >
-          Add Contact
-        </Button>
-      </Form> */}
-      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+      
+      <form class="d-flex justify-content-center align-items-center container " onSubmit={onSubmitForm}>
         <div>
-        <div>
-          <label for="name">Name</label>
-          <input type="text" className="form-control" value={name} onChange={e => setName(e.target.value)}/>
-        </div>
-        <div>
-          <label for="number">Phone Number</label>
-          <input type="number" className="form-control" value={number} onChange={e => setNumber(e.target.value)}/>
-        </div>
-        <div>
-          <label for="email">Email</label>
-          <input type="email" className="form-control" value={email} onChange={e => setEmail(e.target.value)}/>
-        </div>
-        <div>
-          <label for="address">Address</label>
-          <input type="text" className="form-control" value={address} onChange={e => setAddress(e.target.value)}/>
-        </div>
-        <div>
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" className="form-control" value={name} placeholder="Enter name" onChange={e => setName(e.target.value)}/>
+          </div>
+          <div class="form-group">
+            <label for="number">Phone Number</label>
+            <input type="number" className="form-control" value={number} placeholder="Enter contact number" onChange={e => setNumber(e.target.value)}/>
+          </div>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" className="form-control" value={email} placeholder="Enter email" onChange={e => setEmail(e.target.value)}/>
+          </div>
+          <div class="form-group">
+            <label for="address">Address</label>
+            <input type="text" className="form-control" value={address} placeholder="Enter address" onChange={e => setAddress(e.target.value)}/>
+          </div>
           <br></br>
-          <button type="button" class="btn btn-primary" type="submit" onSubmit={onSubmitForm}>Add Contact</button>
-          <Link to="/contacts">
-            <button type="button" class="btn btn-secondary">Cancel</button>
-          </Link>
-        </div>
-
-
-        </div>
+          <div  class="d-grid gap-2 d-md-flex">
+            <button type="button" class="btn btn-primary btn-sm" type="submit" onSubmit={onSubmitForm}>Add Contact</button>
+            <Link to="/contacts">
+              <button type="button" class="btn btn-secondary btn-sm">Cancel</button>
+            </Link>
+          </div>
+          </div>
         
       </form>
     </div>
