1a6a64546ca00298f6619d07503f36cc853fcfee
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-02-16 18:37:38 +0000

    Add about css page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import '../App.css';
+import './About.css';
 
 function About() {
   return (    
-    <h1>This is the About page</h1> 
+    <div className='about-container'> 
+    <h1> The only family organizing app you will need</h1>
+    <p>This is a family organizing app that you and your family can use to stay updated and in sync. Whether its an event you would want your parents to know about or some grocery that you want. Family matters is the place to keep you all in sync.</p>
+    </div>
+    
+
   );
 }
 
