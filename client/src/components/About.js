4c72a844111a10b5e06a3955b80c92cf071039d1
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-02-16 18:33:27 +0000

    remove changes in about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,8 +4,7 @@ import './About.css';
 function About() {
   return (    
     <div className='about-container'> 
-    <h1> From a parent to a parent...</h1>
-    <p>How many times have we forgotten to attend an important family event that </p>
+    
     </div>
     
 
