befc520362b6e9774cf9b80e654f7ec06718086f
EttyDaniel <danieleti@gmail.com>
2022-02-16 23:28:38 -0500

    inserting into db tables events and user_events

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,46 +1,57 @@
-import React, { Component, useState }from 'react';
+import React, { Component, useState, useEffect }from 'react';
 import '../App.css';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import Event_new from './Event_new';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const localizer = momentLocalizer(moment);
 
-//class Events extends Component {
+var myEventsList = [
+  {
+    start: moment().toDate(),
+    end: moment().add(1, "days").toDate(),
+    title: "my event",
+    description: "great"
+  }
+
+];
+
+
+//My state holds an array of events and the selected day clicked by the user
 const Events = () => {
-  const [state, setState] = useState({
-    events: [
-      {
-        // start: moment().toDate(),
-        // end: moment()
-        //   .add(1, "days")
-        //   .toDate(),
-        // title: "Some title"
-      }
-    ],
-    event_modal:false,
-    s_date: ""
+  const [events, setEvents] = useState({
+    events_list: [{}],
+    selected_date: ""//?
   });
 
+  const navigate = useNavigate();
+  const getEvents = () => {
+    return axios.get('/api/events')
+      .then(response => {
+        console.log(response.data.events);
+        setEvents(response.data.events)
+      })
+  }
+
   const createEvent = (e) => {
-    //console.log(e.start);
-    let startDate = moment(e.start.toLocaleString()).format("YYYY-MM-DD");
+    let startDate = moment(e.start.toLocaleString())._i;
+    //let startDate = moment(e.start.toLocaleString()).format("YYYY-MM-DD");
     console.log(startDate);
-    setState({
-      ...state,
-      event_modal: true,
-      s_date: startDate
+    setEvents({
+      ...events,
+      selected_date: startDate
     })
   }
 
-  const closeCreateEvent = () => {
-    setState({
-      ...state,
-      event_modal: false
-    })
-  }
+  // const closeCreateEvent = () => {
+  //   setState({
+  //     ...state,
+  //     event_modal: false
+  //   })
+  // }
 
   const saveEvent = (startDate, title, description) => {
     console.log(startDate, title, description);
@@ -53,27 +64,34 @@ const Events = () => {
       .then(res => {
         console.log("I'm back", res);
       });
-    closeCreateEvent();
+    //closeCreateEvent();
   }
 
-  //render() {
-    return (
-      <>
-      {state.event_modal && <Event_new closeCreateEvent={closeCreateEvent} saveNewEvent={saveEvent} sDate={state.s_date}/>}
+  return (
+    <>
+      {/* {state.event_modal && <EventForm closeCreateEvent={closeCreateEvent} saveNewEvent={saveEvent} sDate={state.s_date}/>} */}
       <div className="App">
-        <Calendar selectable
-          localizer={localizer}
-          defaultDate={new Date()}
-          defaultView="month"
-          //events={this.state.events}
-          events={state.events}
-          onSelectSlot={createEvent}
-          style={{ height: 750, marginLeft: 310, marginRight: 15 }}
-        />
+      <Calendar 
+        selectable
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="month"
+        events={myEventsList}
+        //events={events.events_list}
+        onSelectSlot={createEvent}
+        style={{ height: 750, marginLeft: 310, marginRight: 15 }} // need to move to css file
+      />
       </div>
-      </>
-    );
-  //}
+      <div>
+      <Link to="/eventForm">
+        <button type="button" class="btn btn-info">
+          Add New Event
+        </button>
+      </Link>
+    </div>
+    </>
+  );
 }
 
 export default Events;
+//axios.post('http://localhost:8001/api/events'
