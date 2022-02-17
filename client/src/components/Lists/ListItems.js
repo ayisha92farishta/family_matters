74ea461cf7808a7404296a702c5c6228c650a148
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-02-17 16:43:50 +0000

    Add dropdown menu

--- client/src/components/Lists/ListItems.js
+++ client/src/components/Lists/ListItems.js
@@ -36,6 +36,14 @@ function ListItems(props) {
 //console.log("item names----------------------",itemNames);
   return (
     <>     
+    <div>
+      <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </div>
     <table className="table list-table my-5">
       <thead className="thead-dark">
         <tr>
