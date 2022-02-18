bb4a144d57c5baa33ab1a036282b78f536d34d20
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-02-18 01:44:35 -0500

    Update the meals table, add days table to the schema and insert data

---
+++ server/db/seeds/11_meals.sql
@@ -0,0 +1,13 @@
+INSERT INTO meals (breakfast, lunch, snack, dinner, day_id, account_id) VALUES ('Pancakes','Rice and soup','Chips and salsa','Tortilla wrap', 1, 1);
+
+INSERT INTO meals (breakfast, lunch, snack, dinner, day_id, account_id) VALUES ('Cereals','Noodles and soup','Cookies','Veggie wrap', 2, 1);
+
+INSERT INTO meals (breakfast, lunch, snack, dinner, day_id, account_id) VALUES ('Waffles','Burger','Crakers','Noodles', 3, 1);
+
+INSERT INTO meals (breakfast, lunch, snack, dinner, day_id, account_id) VALUES ('Cheese toast', 'Rice and soup','Cookies','Veggie wrap', 4, 1);
+
+INSERT INTO meals (breakfast, lunch, snack, dinner, day_id, account_id) VALUES ('Cereals','Cup noodles','Muffins','Rice', 5, 1);
+
+INSERT INTO meals (breakfast, lunch, snack, dinner, day_id, account_id) VALUES ('Boiled eggs','Sandwich','Cup cakes','Veggie wrap', 6, 1);
+
+INSERT INTO meals (breakfast, lunch, snack, dinner, day_id, account_id) VALUES ('Milk and bread','Burger','Cookies', 'Pizza', 7, 1);
