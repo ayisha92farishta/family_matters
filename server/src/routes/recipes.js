24e674de57c05e24108195b199d6ded56276083e
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-02-19 17:39:36 -0500

    Update the routes for recipes to include posted_by as another property

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -36,10 +36,10 @@ module.exports = (db) => {
   //Create a new recipe for a user
   router.post("/", (req, res) => {
     console.log('post req.body = ', req.body);
-    const { name, thumbnail_photo_url, preparation_time, cooking_time, serving, ingredients, instructions } = req.body;
+    const { name, thumbnail_photo_url, preparation_time, cooking_time, serving, ingredients, instructions, posted_by } = req.body;
     const userId = req.query.userId;
     const account_id = req.query.accountId;
-    db.query(`INSERT INTO recipes (name , thumbnail_photo_url,  preparation_time, cooking_time, serving, ingredients, instructions, account_id, user_id ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`, [ name, thumbnail_photo_url, preparation_time, cooking_time, serving, ingredients, instructions, account_id, userId ])
+    db.query(`INSERT INTO recipes (name , thumbnail_photo_url,  preparation_time, cooking_time, serving, ingredients, instructions, posted_by, account_id, user_id ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`, [ name, thumbnail_photo_url, preparation_time, cooking_time, serving, ingredients, instructions, posted_by, account_id, userId ])
       .then(data => {
         console.log(data.rows[0]);
         const newRecipe = {
@@ -49,7 +49,8 @@ module.exports = (db) => {
           cooking_time : data.rows[0].cooking_time,
           serving : data.rows[0].serving,
           ingredients : data.rows[0].ingredients,
-          instruction : data.rows[0].instructions
+          instructions : data.rows[0].instructions,
+          posted_by : data.rows[0].posted_by
         }
         res.json( {newRecipe} );
       })
@@ -64,11 +65,11 @@ module.exports = (db) => {
     console.log('edit req.body = ', req.body);
     const recipe_id = req.query.recipeId;
     const userId = req.query.userId;
-    const { name, thumbnail_photo_url, preparation_time, cooking_time, serving, ingredients, instructions } = req.body;
+    const { name, thumbnail_photo_url, preparation_time, cooking_time, serving, ingredients, instructions, posted_by } = req.body;
     //console.log(req.body)
-    const values = [name, thumbnail_photo_url, preparation_time, cooking_time, serving, ingredients, instructions, recipe_id, userId];
+    const values = [name, thumbnail_photo_url, preparation_time, cooking_time, serving, ingredients, instructions, posted_by, recipe_id, userId];
     console.log('values = ', values)
-    db.query(`UPDATE recipes SET name = $1, thumbnail_photo_url = $2, preparation_time = $3, cooking_time = $4, serving = $5, ingredients = $6, instructions = $7 WHERE id = $8 AND user_id = $9 RETURNING *;`, values)
+    db.query(`UPDATE recipes SET name = $1, thumbnail_photo_url = $2, preparation_time = $3, cooking_time = $4, serving = $5, ingredients = $6, instructions = $7, posted_by = $8 WHERE id = $9 AND user_id = $10 RETURNING *;`, values)
       .then(data => {
         console.log('after update = ', data.rows[0]);
         const updatedRecipe = {
@@ -79,7 +80,8 @@ module.exports = (db) => {
           cooking_time : data.rows[0].cooking_time,
           serving : data.rows[0].serving,
           ingredients : data.rows[0].ingredients,
-          instruction : data.rows[0].instructions
+          instructions : data.rows[0].instructions,
+          posted_by : data.rows[0].posted_by
         }
         res.json( {updatedRecipe} );
       })
