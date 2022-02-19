e20969b2e16a94ec61815dfb497a67637c3ceed7
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-02-18 22:37:16 -0500

    Update the meals table to include all meal types and insert data

---
+++ server/db/seeds/10_meals.sql
@@ -0,0 +1,13 @@
+INSERT INTO meals (day, breakfast, lunch, snack, dinner, account_id) VALUES ('Sunday', 'Pancakes','Rice and soup','Chips and salsa','Tortilla wrap', 1);
+
+INSERT INTO meals (day, breakfast, lunch, snack, dinner, account_id) VALUES ('Monday', 'Cereals','Noodles and soup','Cookies','Veggie wrap', 1);
+
+INSERT INTO meals (day, breakfast, lunch, snack, dinner, account_id) VALUES ('Tuesday', 'Waffles','Burger','Crakers','Noodles', 1);
+
+INSERT INTO meals (day, breakfast, lunch, snack, dinner, account_id) VALUES ('Wednesday', 'Cheese toast', 'Rice and soup','Cookies','Veggie wrap', 1);
+
+INSERT INTO meals (day, breakfast, lunch, snack, dinner, account_id) VALUES ('Thursday', 'Cereals','Cup noodles','Muffins','Rice', 1);
+
+INSERT INTO meals (day, breakfast, lunch, snack, dinner, account_id) VALUES ('Friday', 'Boiled eggs','Sandwich','Cup cakes','Veggie wrap', 1);
+
+INSERT INTO meals (day, breakfast, lunch, snack, dinner, account_id) VALUES ('Saturday', 'Milk and bread','Burger','Cookies', 'Pizza', 1);
