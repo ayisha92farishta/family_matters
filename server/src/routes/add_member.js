52a3a934924cec8b4956544f9b34ba3bc9bbc8c9
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-02-20 00:39:03 -0500

    Define a new post route to add a new member to existing account

---
+++ server/src/routes/add_member.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const router = express.Router();
+
+module.exports = (db) => {
+
+  router.post('/', (req, res) => {
+    console.log(req.body);
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const email = req.body.email;
+    const password = req.body.password;
+    let is_primary = false;
+    const account_id = req.query.accountId;
+    const hashedPassword = bcrypt.hashSync(password, 10)
+    const values = [firstName, lastName, email, hashedPassword, is_primary, account_id];
+    
+
+    if (req.body.name === '' || req.body.email === '' || req.body.password === '') {
+      res.statusCode = 400;
+      return res.send('Error : Invalid username or email or password.');
+    }
+    db.query(`INSERT INTO users (first_name, last_name, email, password, is_primary, account_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`, values)
+      .then((data) => {
+        console.log(data.rows[0]);
+        const newUser = {
+          firstName: data.rows[0].first_name,
+          user_id : data.rows[0].id,
+          account_id : data.rows[0].account_id
+        }
+          
+        res.json( newUser );
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+    
+  });
+  return router;
+}
